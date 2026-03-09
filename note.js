1adcfb8daa3ddb3b3f967270d3e89746915484cb
araborg <arababs2015@gmail.com>
2026-03-09 23:14:56 +0100

    intro: 2

--- note.js
+++ note.js
@@ -1,6 +1,15 @@
 /*
 Tech Crush Promises/Syllabus:
-server architecture, RESTful APIs, database design and management, authentication systems, and the principles of writing clean, scalable server-side code
+server architecture, 
+
+RESTful APIs, 
+
+database design and management, 
+
+authentication systems, 
+
+and the principles of writing clean, 
+scalable server-side code
 
 
 
