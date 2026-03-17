9b740165d2a4a8d7c3a0e286c28781a190f0782b
araborg <arababs2015@gmail.com>
2026-03-17 21:37:27 +0100

    tech_crush: 2

---
+++ assignmts/assignmt_1.js
@@ -0,0 +1,15 @@
+// Online Bookstore Order
+
+// Task 1: Create Variables
+
+const customerName = "Alice";
+const customerAge = 32;
+const bookTitle = "How to get what you want";
+const bookPrice = 2500;
+const quantity = 300;
+const isStudent = true;
+
+// Task 2: Use Arithmetic Operators
+
+const totalPrice = bookPrice * quantity;
+console.log(totalPrice);
