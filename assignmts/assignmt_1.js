cb47da62c1f0533185b9038ae16e0e9ad000dc7d
araborg <arababs2015@gmail.com>
2026-03-17 21:43:01 +0100

    tech_crush: 4

--- assignmts/assignmt_1.js
+++ assignmts/assignmt_1.js
@@ -15,6 +15,19 @@ const totalPrice = bookPrice * quantity;
 console.log(totalPrice);
 
 // Task 3: Use Comparison Operators
+
+// Check if the customer is 18 years or older.
 if (customerAge > 18) {
 	console.log("You are older than 18 years");
+}
+
+// Check if the quantity of books is more than 3.
+if (quantity > 3) {
+	console.log("You have more than 3 books");
+}
+// Check if the book price is equal to 5000.
+if (bookPrice === 5000) {
+	console.log("The book price is equal to $5000");
+} else {
+	console.log("The book price is less than $5000");
 }
