bc82fe1918a9f8378868ad980107a86623a86dec
araborg <arababs2015@gmail.com>
2026-03-29 16:17:19 +0100

    2nd assignmt: 2

---
+++ assignmts/assignmt_2.js
@@ -0,0 +1,9 @@
+const str = "Nigeria is awesome";
+/*
+- Given const str = "Nigeria is awesome";
+- Extract "Nigeria" using substring().
+- Extract "awesome" using slice().
+- Get "is" using slice().
+
+*/
+console.log(str);
