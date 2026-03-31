4eb2aae7ce0d722f02eee25501e1c2f0e41da0ab
araborg <arababs2015@gmail.com>
2026-03-31 11:47:15 +0100

    assignmt_3: 1

--- assignmts/assignmt_2.js
+++ assignmts/assignmt_2.js
@@ -1,26 +1,20 @@
 // Que: 1
-
 const str = "Nigeria is awesome";
-
 // Extract "Nigeria" using substring().
 const nigeria = str.substring(0, 7);
 console.log(nigeria);
-
 // Extract "awesome" using slice().
 const awesome = str.slice(-7);
 console.log(awesome);
-
 // Get "is" using slice().
 const is = str.slice(-10, -7);
 console.log(is);
 
 // Que: 2
 const names = "John,Jane,Doe";
-
 // Split into an array using comma ,.
 const arrNames = names.split(",");
 console.log(arrNames);
-
 // Join with space instead: "John Jane Doe".
 const strNames = arrNames.join(" ");
 console.log(strNames);
@@ -30,22 +24,18 @@ const path = "/user/home/docs";
 // Split by / → ["", "user", "home", "docs"]
 const splitedPath = path.split("/");
 console.log(splitedPath);
-
 // Get "home" using split & index.
 const home = path.split("/")[2];
 console.log(home);
 
 // Ques: 3
 const email = "user@example.com";
-
 // Get username (user) using split().
 const username = email.split("@")[0];
 console.log(username);
-
 // Check if it's a .com domain using endsWith().
 const isDotCom = email.endsWith(".com");
 console.log(isDotCom);
-
 // Replace @ with AT using replace().
 const replaceAt = email.replace("@", "AT");
 console.log(replaceAt);
