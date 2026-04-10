1a3b0370ad63461823e8eb7f9bc07c77b1d7ca3b
araborg <arababs2015@gmail.com>
2026-04-10 17:04:32 +0100

    tech_crush: 2

--- assignmts/assignmt_4.js
+++ assignmts/assignmt_4.js
@@ -1,7 +1,16 @@
 /*
+1. 
 All students: Create a 3-page Google Doc report on version 
 control systems (types, advantages, disadvantages, differences, 
 examples), submit via portal by the deadline announced on the 
 portal.
+
+
+2.
+All students: Push all class files to a new GitHub repository 
+and ensure the repository link is submitted to Esther via the 
+portal for grading.
+
+
 
 */
